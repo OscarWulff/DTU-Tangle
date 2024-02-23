15ed6347d5cd6923508a81b8ce596903a10319bb
soender2 <s214925@dtu.dk>
2024-02-23 10:01:41 +0100

    Defining the files and methods

--- Model/DataSetBinaryQuestionnaire.py
+++ Model/DataSetBinaryQuestionnaire.py
@@ -4,10 +4,17 @@ def dimension_reduction_binary():
     This function is used to reduce the dimension of binary questionnaires data set 
     
     Parameters:
-
+    Takes a csv-file of zeros and ones. Zero represents "No" and ones represent "yes" to a question. 
+    Each row represent object.
+    Example of a file: 
+    "1,0,0,0,0,1,1,1,1
+     0,1,0,0,1,1,1,0,0
+     ....
+     ...
+    "
 
     Returns:
-    Eigenvectors created from PCA 
+    Eigenvectors and Projections of the PCA
     """
     pass
 
@@ -17,10 +24,10 @@ def cost_function_binary():
     This function is used to calculate the cost of cuts for binary questionnaires data set 
     
     Parameters:
-    
+    cuts of the dataset
 
     Returns:
-
+    cost of each cut
     """
     pass
 
@@ -30,8 +37,18 @@ def cut_generator_binary():
     This function is used to generate the cuts for binary questionnaires data set
     
     Parameters:
+    Takes a csv-file of zeros and ones. Zero represents "No" and ones represent "yes" to a question. 
+    Each row represent object.
+    Example of a file: 
+    "1,0,0,0,0,1,1,1,1
+     0,1,0,0,1,1,1,0,0
+     ....
+     ...
+    "
 
     Returns:
+    cuts of the dataset
+    
     """
     pass
 
--- Model/DataSetGraph.py
+++ Model/DataSetGraph.py
@@ -3,9 +3,10 @@ def cost_function_feature_based():
     This function is used to calculate the cost of cuts for graph data set
     
     Parameters:
+    cuts of the dataset
 
     Returns:
-
+    cost of the cuts
     """
     pass
 
@@ -14,9 +15,10 @@ def cut_generator_feature_based():
     This function is used to generate the cuts for graph data set
     
     Parameters:
+    the graph dataset
 
     Returns:
-    
+    cuts of the dataset
     """
     pass
 
