23c8f92d956139d4ac43c0a4359fb772e7c9c82d
H3lsoe <morten@helsoe.dk>
2024-04-05 13:53:28 +0200

    view for binary updated

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,3 +1,16 @@
 
-# for cut in data_cuts.cuts:
-#     print(cut.A, " ", cut.Ac, " ", cut.cost)
+
+# tree = create_searchtree(data_cuts)
+
+# # print_tree(tree)
+# new_new_tree = condense_tree(tree)
+# # print_tree(new_new_tree)
+# contracting_search_tree(new_new_tree)
+
+
+# soft = soft_clustering(tree)
+# hard = hard_clustering(soft)
+
+# color_dict = generate_color_dict(hard)
+
+# print(color_dict)
